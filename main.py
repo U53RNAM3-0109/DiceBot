0c8dc3040efe0a30239852f71b50105166ccbb66
U53RNAM3-0109 <95925165+U53RNAM3-0109@users.noreply.github.com>
2021-12-11 13:44:57 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import DiceInator3000 as discBot
 import os
 
 if __name__ == "__main__":
-    discBot.client.run('OTAzOTcyNjA2MTQ1ODgwMDk0.YX0wGA.NF-3PgaImjd2jKS6_KszNe593YM')
+    discBot.client.run(os.environ['token'])
